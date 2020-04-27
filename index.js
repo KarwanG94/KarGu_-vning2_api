ec51f43227f8f99dbfaa0d2c9faa9efa43502139
Karwan Gulnezer <karwan_gulnezer@hotmail.com>
2020-04-27 17:07:03 +0200

    get a user and post

--- index.js
+++ index.js
@@ -3,8 +3,49 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function(req, res){
-res.send('Hello World!')
+app.get('/users/1', function(req, res){
+res.send({
+    "address": {
+      "geo": {
+        "lat": -37.3159,
+        "lng": 81.1496
+      },
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874"
+    },
+    "_id": "5e806d9f42fbde006b6b9ec5",
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "__v": 0
+  })
+})
+
+app.post('/users', function(req, res){
+    res.status(201)
+    res.send(
+        {
+            "address": {
+              "geo": {
+                "lat": 0,
+                "lng": 0
+              },
+              "street": "string",
+              "suite": "string",
+              "city": "string",
+              "zipcode": "string"
+            },
+            "_id": "5ea6f22f13ee69002583acc1",
+            "name": "string",
+            "username": "string",
+            "email": "string",
+            "id": 390,
+            "__v": 0
+          }
+    )
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
